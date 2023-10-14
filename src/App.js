771051270895f22037c0c35c9873f4eed3280b75
Blue260910 <rm99667@fiap.com.br>
2023-10-14 00:32:11 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from './components/Header/Header';  
+import Footer from './components/Footer/Footer';
+
+import Home from './components/Home/Home';
+import Apoio from './components/Apoio/Apoio';
+import Login from './components/Login/Login';
+import Cadastro from './components/Cadastro/Cadastro';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/pe-dagua-next/Home" component={Home} />
+        <Route path="/pe-dagua-next/Apoio" component={Apoio} />
+        <Route path="/pe-dagua-next/Login" component={Login} />
+        <Route path="/pe-dagua-next/Cadastro" component={Cadastro} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
